0b41c6e5a9a796b40edfd95ef77de51ced3f95ae
prasanth-andinolabs <leelanakka@Leelaprasanths-MacBook-Pro.local>
2026-02-06 12:13:53 +0530

    chore: fixes build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(async () => {
+  const { VitePWA } = await import('vite-plugin-pwa');
+  return {
   plugins: [
     react(),
     VitePWA({
@@ -55,4 +56,5 @@ export default defineConfig({
       '/api': 'http://localhost:5174',
     },
   },
+  };
 });
